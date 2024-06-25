6543226ac202dfd71a31701497985938251ca499
장소현 <sssaa123@naver.com>
2024-06-25 12:00:59 +0900

    aws테스트

---
+++ src/test/java/com/appEC2/AppEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appEC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
